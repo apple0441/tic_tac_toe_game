994bb6a9b84c014e0498706487db5a101b00bf36
robertSawlton <robertsawlton33@gmail.com>
2023-04-11 19:37:41 +0000

    Add base function that displays tic tac toe field

--- run.py
+++ run.py
@@ -13,3 +13,20 @@ print("""
 
 """)
 
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+field = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+horizontal = 3
+vertical = 3
+
+def displayField():
+  """ 
+  A function that sets the field that iterates through,
+  Sets the boundaries and core positions of the game
+  """ 
+  for x in range(horizontal):
+    print('\n+---+---+---+')
+    print('|', end='')
+    for y in range(vertical):
+      print('', field[x][y], end=' |')
+  print('\n+---+---+---+')
+
